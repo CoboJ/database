919f88aca1091bc23258296166328fcc1494014c
Jorge Cobo <cjorge@Jorges-Mac-mini.local>
2022-04-22 09:18:01 -0400

    Persistence to disk db & testing new db.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
-#include <iostream>
-#include "cstring"
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
+#include <unistd.h>
+#include <cerrno>
+#include <fcntl.h>
 
 typedef struct
 {
@@ -55,8 +60,14 @@ const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 typedef struct {
-    uint32_t num_rows;
+    int file_descriptor;
+    uint32_t file_length;
     void* pages[TABLE_MAX_PAGES];
+} Pager;
+
+typedef struct {
+    uint32_t num_rows;
+    Pager* pager;
 } Table;
 
 void print_row(Row* row)
@@ -66,54 +77,99 @@ void print_row(Row* row)
 
 void serialize_row(Row* source, void* destination)
 {
-    memcpy((char*)destination + ID_OFFSET, &(source->id), ID_SIZE);
-    memcpy((char*)destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-    memcpy((char*)destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+    memcpy((unsigned char*)destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy((unsigned char*)destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy((unsigned char*)destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
 }
 
 void deserialize_row(void* source, Row* destination)
 {
-    memcpy(&(destination->id), (char*)source + ID_OFFSET, ID_SIZE);
-    memcpy(&(destination->username), (char*)source + USERNAME_OFFSET, USERNAME_SIZE);
-    memcpy(&(destination->email), (char*)source + EMAIL_OFFSET, EMAIL_SIZE);
+    memcpy(&(destination->id), (unsigned char*)source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), (unsigned char*)source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), (unsigned char*)source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+void* get_page(Pager* pager, uint32_t page_num)
+{
+    if (page_num > TABLE_MAX_PAGES)
+    {
+        printf("Tried to fetch page number out of bounds. %d > %d\n", page_num, TABLE_MAX_PAGES);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pager->pages[page_num] == nullptr)
+    {
+        // Cache miss. Allocate memory and load from file.
+        void* page = malloc(PAGE_SIZE);
+        uint32_t num_pages = pager->file_length / PAGE_SIZE;
+
+        // We might save a partial page at the end of the file
+        if(pager->file_length % PAGE_SIZE)
+        {
+            num_pages += 1;
+        }
+
+        if (page_num <= num_pages)
+        {
+            lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+            ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+            if(bytes_read == -1)
+            {
+                printf("Error reading file: %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        pager->pages[page_num] = page;
+    }
+
+    return pager->pages[page_num];
 }
 
 void* row_slot(Table* table, uint32_t row_num)
 {
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void *page = table->pages[page_num];
-    if (page == nullptr)
-    {
-        // Allocate memory only when we try to access page
-        page = table->pages[page_num] = malloc(PAGE_SIZE);
-    }
 
+    void *page = get_page(table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
-    return (char*)page + byte_offset;
+    return (unsigned char*)page + byte_offset;
 }
 
-Table* new_table()
+Pager* pager_open(const char* filename)
 {
-    auto* table = (Table*)malloc(sizeof(Table));
-    table->num_rows = 0;
+    int fd = open(filename, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR);
 
-    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++)
+    if (fd == -1)
     {
-        table->pages[i] = nullptr;
+        printf("Unable to open file\n");
+        exit(EXIT_FAILURE);
     }
+
+    off_t file_length = lseek(fd, 0, SEEK_END);
+
+    auto* pager = (Pager*)malloc(sizeof(Pager));
+    pager->file_descriptor = fd;
+    pager->file_length = file_length;
+
+    for (auto & page : pager->pages)
+    {
+        page = nullptr;
+    }
+
+    return pager;
+}
+
+Table* db_open(const char* filename)
+{
+    Pager* pager = pager_open(filename);
+    uint32_t num_rows = pager->file_length / ROW_SIZE;
+
+    auto* table = (Table*)malloc(sizeof(Table));
+    table->pager = pager;
+    table->num_rows = num_rows;
 
     return table;
-}
-
-void free_table(Table* table)
-{
-    for (int i = 0; table->pages[i]; i++)
-    {
-        free(table->pages[i]);
-    }
-
-    free(table);
 }
 
 typedef struct {
@@ -129,6 +185,81 @@ InputBuffer* new_input_buffer()
     input_buffer->input_length = 0;
 
     return input_buffer;
+}
+
+void pager_flush(Pager* pager, uint32_t page_num, uint32_t size)
+{
+    if(pager->pages[page_num] == nullptr)
+    {
+        printf("Tried to flush null page\n");
+        exit(EXIT_FAILURE);
+    }
+
+    off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+
+    if(offset == -1)
+    {
+        printf("Error seeking: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t bytes_written = write(pager->file_descriptor, pager->pages[page_num], size);
+
+    if(bytes_written == -1)
+    {
+        printf("Error writing: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void db_close(Table* table)
+{
+    Pager* pager = table->pager;
+    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
+
+    for (uint32_t i = 0; i < num_full_pages; i++)
+    {
+        if (pager->pages[i] == nullptr)
+        {
+            continue;
+        }
+        pager_flush(pager, i, PAGE_SIZE);
+        free(pager->pages[i]);
+        pager->pages[i] = nullptr;
+    }
+
+    // There may be a partial page to write to the end of the file
+    // This should not be needed after we switch to a B-tree
+    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+    if (num_additional_rows > 0)
+    {
+        uint32_t page_num = num_full_pages;
+        if (pager->pages[page_num] != nullptr)
+        {
+            pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+            free(pager->pages[page_num]);
+            pager->pages[page_num] = nullptr;
+        }
+    }
+
+    int result = close(pager->file_descriptor);
+    if (result == -1)
+    {
+        printf("Error closing db file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (auto & i : pager->pages) {
+        void* page = i;
+        if (page)
+        {
+            free(page);
+            i = nullptr;
+        }
+    }
+
+    free(pager);
+    free(table);
 }
 
 void print_prompt() { printf("db > "); }
@@ -159,7 +290,7 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table)
     if (strcmp(input_buffer->buffer, ".exit") == 0)
     {
         close_input_buffer(input_buffer);
-        free_table(table);
+        db_close(table);
         exit(EXIT_SUCCESS);
     }
     else
@@ -170,39 +301,39 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table)
 
 PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) 
 {
-  statement->type = STATEMENT_INSERT;
+    statement->type = STATEMENT_INSERT;
 
-  char* keyword = strtok(input_buffer->buffer, " ");
-  char* id_string = strtok(NULL, " ");
-  char* username = strtok(NULL, " ");
-  char* email = strtok(NULL, " ");
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(nullptr, " ");
+    char* username = strtok(nullptr, " ");
+    char* email = strtok(nullptr, " ");
 
-  if (id_string == NULL || username == NULL || email == NULL) 
-  {
-    return PREPARE_SYNTAX_ERROR;
-  }
+    if (id_string == nullptr || username == nullptr || email == nullptr)
+    {
+        return PREPARE_SYNTAX_ERROR;
+    }
 
-  int id = atoi(id_string);
-  if (id < 0)
-  {
-      return PREPARE_NEGATIVE_ID;
-  }
+    int id = atoi(id_string);
+    if (id < 0)
+    {
+        return PREPARE_NEGATIVE_ID;
+    }
 
-  if (strlen(username) > COLUMN_USERNAME_SIZE) 
-  {
-    return PREPARE_STRING_TOO_LONG;
-  }
+    if (strlen(username) > COLUMN_USERNAME_SIZE)
+    {
+        return PREPARE_STRING_TOO_LONG;
+    }
   
-  if (strlen(email) > COLUMN_EMAIL_SIZE) 
-  {
-    return PREPARE_STRING_TOO_LONG;
-  }
+    if (strlen(email) > COLUMN_EMAIL_SIZE)
+    {
+        return PREPARE_STRING_TOO_LONG;
+    }
 
-  statement->row_to_insert.id = id;
-  strcpy(statement->row_to_insert.username, username);
-  strcpy(statement->row_to_insert.email, email);
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
 
-  return PREPARE_SUCCESS;
+    return PREPARE_SUCCESS;
 }
 
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
@@ -261,7 +392,15 @@ ExecuteResult execute_statement(Statement* statement, Table* table)
 
 int main(int argc, char* argv[])
 {
-    Table* table = new_table();
+    if(argc < 2)
+    {
+        printf("Must supply a database filename.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char* filename = argv[1];
+    Table* table = db_open(filename);
+
     InputBuffer* input_buffer = new_input_buffer();
 
     while (true)
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,7 +1,11 @@
 describe 'database' do
+  before do
+    `rm -rf test.db`
+  end
+
   def run_script(commands)
     raw_output = nil
-    IO.popen("./db", "r+") do |pipe|
+    IO.popen("./db test.db", "r+") do |pipe|
       commands.each do |command|
         pipe.puts command
       end
@@ -25,6 +29,27 @@ describe 'database' do
       "db > (1, user1, person1@example.com)",
       "Executed.",
       "db > ",
+    ])
+  end
+
+  it `keeps data after closing connection` do
+    resul1 = run_script([
+        "insert 1 user1 person1@example.com",
+        ".exit",
+    ])
+    expect(resul1).to match_array([
+        "db > Executed.",
+        "db > ",
+    ])
+
+    resul2 = run_script([
+        "select",
+        ".exit",
+    ])
+    expect(resul2).to match_array([
+        "db > (1, user1, person1@example.com)",
+        "Executed.",
+        "db > ",
     ])
   end
 
