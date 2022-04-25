2b15d1f8f1c68823426a843772d56a29ee4d8295
Jorge Cobo <cjorge@Jorges-Mac-mini.local>
2022-04-25 11:36:31 -0400

    segmentation fault bug fixed

--- main.cpp
+++ main.cpp
@@ -115,22 +115,22 @@ const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_
 
 uint32_t* leaf_node_num_cells(void* node)
 {
-    return (unsigned int*)node + LEAF_NODE_NUM_CELLS_OFFSET;
+    return (uint32_t*)node + LEAF_NODE_NUM_CELLS_OFFSET;
 }
 
 void* leaf_node_cell(void* node, uint32_t cell_num)
 {
-    return (unsigned char*)node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
+    return (uint32_t*)node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
 }
 
 uint32_t* leaf_node_key(void* node, uint32_t cell_num)
 {
-    return (unsigned int*)leaf_node_cell(node, cell_num);
+    return (uint32_t*)leaf_node_cell(node, cell_num);
 }
 
 void* leaf_node_value(void* node, uint32_t cell_num)
 {
-    return (unsigned char*)leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
+    return (uint32_t*)leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
 }
 
 void print_constants()
